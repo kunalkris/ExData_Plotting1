d27dcfca295f60602278f833bdc2d063e946e2dc
Kunal Krishna <kunalkris@hotmail.com>
2015-02-07 14:43:18 +0530

    Plot 3

---
+++ plot3.r
@@ -0,0 +1,10 @@
+household_power_consumption <- read.table("~/Documents/Programming/r/exdata/household_power_consumption.txt", sep=";", header = TRUE, na.strings="?")
+household_power_consumption <- household_power_consumption[household_power_consumption$Date %in% c('1/2/2007', '2/2/2007'),]
+household_power_consumption$DateTime <- strptime(paste(household_power_consumption$Date, household_power_consumption$Time),"%d/%m/%Y %H:%M:%S")
+png(filename = './exdata/ExData_plotting1/plot3.png', width = 480, height = 480)
+plot(household_power_consumption$DateTime, household_power_consumption$Sub_metering_1, type = 'n', xlab = '', ylab = 'Energy sub metering')
+lines(household_power_consumption$DateTime, household_power_consumption$Sub_metering_1, type = 'l', col = 'black')
+lines(household_power_consumption$DateTime, household_power_consumption$Sub_metering_2, type = 'l', col = 'red')
+lines(household_power_consumption$DateTime, household_power_consumption$Sub_metering_3, type = 'l', col = 'blue')
+legend('topright', legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), col = c('black', 'red', 'blue'), lty='solid')
+dev.off()
